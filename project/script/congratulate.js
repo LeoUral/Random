cb7f73a49cd6ab47b7d96eeb63752221ef8feda0
Leonid <leoural66@yandex.ru>
2020-02-06 20:42:40 +0500

    мелкие исправления

--- project/script/congratulate.js
+++ project/script/congratulate.js
@@ -1,8 +1,8 @@
 "use strict"
 
-let congratulateLink = document.querySelector('.data-title-link');
-let congratulateLinkMain = document.querySelector('.congratulate_main_lnk');
+
 let findings = {
+    id: 0,
     who: "",
     congratulate: "",
     number: 0,
@@ -11,10 +11,13 @@ let findings = {
 
 const renderFirst = `
 <p class="main-block-data-menu">Ваша случайность из категории:<span class="cat-sel">Поздравления</span><span class="cat-settings" onclick="rendGo();" style="cursor: pointer;">Настроить фильтр</span><span class="next-random" onclick="firstRnd();" style="cursor: pointer;">Следующая случайность</span></p>
-        <div class="main-block-data-primary"></div>`
+        <div class="main-block-data-primary"></div>
+        <div class="congratulate_text"></div>
+        <div class="other-cat">Кроме поздравлений наш генератор выдаёт варианты из <a href="#" class="link-in-text">других категорий</a>, например, &laquo;<a href="#" class="link-in-text">Исторический факт</a>&raquo;</div>        
+        `;
 
 const renderGo = `
-<p class="main-block-data-menu">Ваша случайность из категории:<span class="cat-sel">Поздравления</span><span class="cat-settings">Настроить фильтр</span><span class="next-random">Следующая случайность</span></p>
+<p class="main-block-data-menu">Ваша случайность из категории:<span class="cat-sel">Поздравления</span><span class="cat-settings" onclick="rendGo();" style="cursor: pointer;">Настроить фильтр</span><span class="next-random" onclick="firstRnd();" style="cursor: pointer;">Следующая случайность</span></p>
         <div class="main-block-data-primary">
             <div class="congratulate_them">
                 <div class="congratulate_him who" data-one="him">поздравить его</div>
@@ -50,7 +53,7 @@ const renderGo = `
                     <div class="another_group congr" data-two="another">прочее</div>
                 </div>
             </div>
-            <div class="comgratulate_text"></div>
+            <div class="congratulate_text"></div>
 
             <style>
                 .main-block-data-primary {
@@ -76,7 +79,7 @@ const renderGo = `
                 }
                 
                 .comgratulate_text {
-                    width: 50%;
+                    width: 45%;                    
                 }
                 
                 .who,
@@ -114,30 +117,41 @@ const renderGo = `
 `;
 
 //грузим модуль в дивы
-let btnGo = document.querySelector('.btn_go'); // кнопка пуска, загрузки, старта
-let mainBlock = document.querySelector('.main-block-data');
-btnGo.addEventListener('click', function () {
-    mainBlock.innerHTML = renderFirst;
-    congratulate.init();
-    setTimeout(firstRnd, 500);//время задержки, что бы успел выполниться callback у запроса
-});
+let congratulateLink = document.querySelector('.data-title-link');
+let congratulateLinkMain = document.querySelector('.congratulate_main_lnk');
+let btnGo = document.querySelector('.btn_go'); // кнопка пуска, загрузки, старта в пробном варианте
+if (congratulateLink) {
+    runProgr(congratulateLink);
+} else if (congratulateLinkMain) {
+    runProgr(congratulateLinkMain);
+} else if (btnGo) {
+    runProgr(btnGo);
+}
+//грузим модуль Первая загрузка со случайным поздравлением
+function runProgr(event) {
+    let mainBlock = document.querySelector('.main-block-data');//место вставки
+    event.addEventListener('click', function () {
+        mainBlock.innerHTML = renderFirst;
+        congratulate.init();
+        setTimeout(firstRnd, 500);//время задержки, что бы успел выполниться callback у запроса
+    });
+};
 
-//грузим модуль в дивы
+//грузим модуль с фильтром выбора поздравлений
 function rendGo() {
     let mainBlock = document.querySelector('.main-block-data');
     mainBlock.innerHTML = renderGo;
     congratulate.init();
 };
 
+//первое случайное поздравление и также вывод "следующая случайность"
 function firstRnd() {
     let firstNumber = Math.floor(Math.random() * congratulate.data.length);
-    console.log(congratulate.data);
+    findings.id = firstNumber;
     let firstText = congratulate.data[firstNumber].congratulate;
-    let firstBlock = document.querySelector('.main-block-data-primary');
-    firstBlock.innerHTML = firstText;
+    console.log(findings);
+    congratulate.renderText(firstText);
 }
-
-
 
 class Congratulate {
     constructor() {
@@ -145,10 +159,13 @@ class Congratulate {
         this.alreadyViewedIds = [];
     }
 
+    //слушаем нажатие - выбор пола, затемняем не активные кнопки
     congratulateSomeone() {
         let who = document.querySelectorAll('.who');
+
         who.forEach(function (whoBtn) {
             whoBtn.addEventListener('click', function (event) {
+
                 let whoWas = event.srcElement.dataset.one;
                 findings.who = whoWas;
                 console.log(whoWas);
@@ -187,16 +204,19 @@ class Congratulate {
         });
     }
 
+    //слушаем нажатие выбора темы поздравления
     subjectCongratulations() {
         let congr = document.querySelectorAll('.congr');
+
         congr.forEach(function (congrBtn) {
             congrBtn.addEventListener('click', function (ev) {
-                let congrWhich = ev.srcElement.dataset.two;//определяем значение класса data-two
-                findings.congratulate = congrWhich;
 
-                //ниже блок определения повторов
+                findings.congratulate = ev.srcElement.dataset.two; //определяем значение класса data-two
+
+                // блок рандома и определения повторов
                 do {
-                    findings.number = Math.floor(Math.random() * 4);// добавить в базу рандомное значение   
+                    findings.number = Math.floor(Math.random() * 4);// добавить в базу рандомное значение  
+
                 } while (congratulate.alreadyViewedIds.includes(findings.number)); //проверяем случайность на повтор
 
                 congratulate.alreadyViewedIds = [...congratulate.alreadyViewedIds, findings.number]; // добавляем случайности в массив
@@ -210,6 +230,7 @@ class Congratulate {
                 console.log(findings.congratulate + " " + findings.number);
                 console.log(findings);
 
+                //в объект findings помещаем случайное поздравление согалсно фильтра отбора
                 for (let i = 0; i < congratulate.data.length; i++) {
                     if (congratulate.data[i].who === findings.who && congratulate.data[i].theme === findings.congratulate && congratulate.data[i].randomId === findings.number) {
                         findings.congrRnd = congratulate.data[i].congratulate;
@@ -217,12 +238,46 @@ class Congratulate {
                         console.log(this.data);
                         console.log(findings.congrRnd);
 
-                        //рендер поздравления
-                        let position = document.querySelector('.comgratulate_text');
-                        position.innerHTML = findings.congrRnd;
+                        // //рендер поздравления
+                        // let position = document.querySelector('.congratulate_text');
+                        // position.innerHTML = findings.congrRnd;
+
+                        congratulate.renderText(findings.congrRnd);
                     }
                 };
             });
+        });
+    }
+
+    //рендер поздравления
+    renderText(event) {
+        let position = document.querySelector('.congratulate_text');
+        position.innerHTML = event;
+    }
+
+    //Функция AJAX получения рандомного 
+    getRndCongratulate() {
+        $.post({
+            url: '/index.php',
+            data: {
+                apiMethod: 'getRndCongratulate',
+                postData: {
+                    // id: findings.id, //он нежун только для первых случайных поздравлений.
+                    who: findings.who,
+                    theme: findings.congratulate,
+                    randomId: findings.number,
+                    congratulate: findings.congrRnd // нужно ли его отправлять? мне его получить надо
+                }
+            },
+            success: function (data) {
+
+                data = JSON.parse(data);
+                if (data.result === "OK") {
+                    congratulate.data = data.rnd;
+                } else {
+                    console.log("ERROR_GET_CONGRATULATE");
+                }
+            }
         });
     }
 
@@ -246,8 +301,9 @@ class Congratulate {
         this.congratulateSomeone();
         this.subjectCongratulations();
         this._requestCongratulate();
+
     }
 }
 
 let congratulate = new Congratulate();
-// congratulate.init();
+
